7ffb081208de520f2b05061a954ceb553b43a2f3
Harshit Prajapati <harshitprajapati5114@gmail.com>
2024-01-10 17:52:36 +0530

    changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,7 @@ const Header = (
     <>
     <div className={`flex flex-wrap  justify-between h-min mt-0 w-full font-mono bg-black text-white`}>
     <div className='flex flex-wrap`'  id='imgg'>
-    <ImgLoader src="../../hars.JPG" alt="nice facing some trouble" name={name} descp={descp} className={`h-[4.5rem] rounded-full m-[0.75rem]
+    <ImgLoader src="../../hars.jpg" alt="nice facing some trouble" name={name} descp={descp} className={`h-[4.5rem] rounded-full m-[0.75rem]
       focus-within:animate-none  animate-pulse`}/>
     
     </div>
