662066a560413d4fe1a4cccf77df6996fc9e3981
Harshit Prajapati <harshitprajapati5114@gmail.com>
2024-01-10 17:38:47 +0530

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import Header from './components/Header';
+import '../hars.jpg'
 import { RouterProvider } from 'react-router-dom';
 import router from './routes/routes';
 import { Provider } from 'react-redux';
