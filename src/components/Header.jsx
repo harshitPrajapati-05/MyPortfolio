06e8c21e06534f831c3f70fd8fcaae58268e9462
Harshit Prajapati <harshitprajapati5114@gmail.com>
2024-01-13 15:05:42 +0530

    ui changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,7 +15,7 @@ const Header = (
 
   return (   
     <>
-    <div className={`flex flex-wrap  justify-between h-min mt-0 w-full font-mono bg-transparent text-white`}>
+    <div className={`flex flex-wrap  justify-between h-min  w-full font-mono bg-transparent text-white`}>
     <div className='flex  font-mono text-[#F5AC0C]  m-2'>
     <Link to="https://www.instagram.com/harshit_prajapati04/">
     {userName!==""?<ImgLoader src={`../hars.jpg`} className={`h-20 rounded-full`} name={name} descp={descp}/>:null}
@@ -23,9 +23,9 @@ const Header = (
     </div>
     <ul className={`flex flex-wrap gap-[1.75rem]`}>
     {contexts.map((context ,index)=>(
-     <li    key={index} className='mt-1 mx-[1.25rem] text-lg font-bold '>
+     <li    key={index} className='mt-1 mx-[1.25rem] text-lg font-mono font-semibold '>
       <NavLink  to={toMaker(`${context}`)}
-      className={({isActive})=>`${isActive? "text-grey glassBox bg-transparent shadow-[1px_1px_15px_1px_rgba(146,46,227,255)]   ":"  item hover:transition-all hover:shadow-[5px_5px_1px_0px_rgba(146,46,227,255)]"}    hover:text-white hover:bg-transparent transition-shadow   flex rounded-lg p-[0.25rem] my-2`}
+      className={({isActive})=>`${isActive? "text-grey glassBox bg-transparent shadow-[1px_1px_15px_1px_rgba(146,46,227,255)]   ":"  item hover:transition-all hover:shadow-[5px_5px_1px_0px_rgba(146,46,227,255)]"}    hover:text-white hover:bg-transparent transition-shadow   flex rounded-lg p-[0.25rem] my-5`}
       >@{context}</NavLink>
      </li>
     ))}
--- src/components/ImgLoader.jsx
+++ src/components/ImgLoader.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { IsOnline } from "../utils";
 
 
@@ -6,37 +7,22 @@ import { IsOnline } from "../utils";
 const ImgLoader = ({ src, alt, className ,name, descp }) => {
 
 
-  const imageDiv = document.getElementById("profileImg");
-  const popUp = document.createElement("span");
-  popUp.innerHTML=' <h1><</h1><h2>Click for Social Media </h2>'
-  popUp.id = "popUp";
-  popUp.className="jello-horizontal"
-  
-  imageDiv?.addEventListener("mousemove", () => {
-    if (!document.body.contains(popUp)) {
-      document.body.append(popUp);
-      
-      for (let i = 2; i < 5; i++) {
-        const spans = document.querySelectorAll("span");
-        i < spans.length ? spans[i].remove() : null;
-      }
-  
-      setTimeout(() => {
-        popUp.remove();
-      }, 2000);
-    }
-  });
-  
-  
-  
+  const [showPopup, setShowPopup] = React.useState(false);
+
+  const handleMouseMove = () => { setShowPopup(true);
+    setTimeout(() => {
+      setShowPopup(false);
+    }, 2500);
+  };
 
 
   return IsOnline() ? (
     <>
-    <div className='mt-1 animate-pulse  flex  flex-focus:animate-none transition-all  text-[#CC6600] ' id='profileImg'>
+    <div  onMouseMove={handleMouseMove} className='mt-0 animate-pulse  flex focus-within:animate-none transition-all  text-[#CC6600] ' id='profileImg'>
+    {showPopup && (<span className=" transition-all animate-none flex ml-[17rem] mt-[2rem] glassBox  popUp absolute jello-horizontal text-white font-mono" >{`<CLICK ME FOR SOCIAL`}</span>)}
     <img src={src}  alt={alt} className={className} />
-    <span className='flex flex-col justify-center'>
-    <h2 >{name}</h2>
+    <span className=' font-semibold animate-none rounded-none glassBox h-min mt-3 p-1'>
+    <h2 className="hover:animate-none p-[1px] ">{name}</h2>
     <h3>{descp}</h3>
     </span>
     </div>
