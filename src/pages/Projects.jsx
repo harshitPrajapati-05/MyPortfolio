a78d55e4669016202bd33fa31e13f4f1ef12c6fb
Harshit Prajapati <harshitprajapati5114@gmail.com>
2024-01-13 22:59:57 +0530

    changes

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,26 +3,32 @@ import React, { useState } from 'react';
 
 const Projects = ({ projects = ["redux", "react"] }) => {
   const [isMouseOver, setIsMouseOver] = useState(null);
-  const reduxSubFolderRef = document.getElementById("reduxsubFolder")
+  
 
   return (
     <>
-      <div className='font-mono capitalize w-[60rem] projectList glassBox h-min mx-auto my-12 '>
-        <ul className='flex h-min gap-4 p-6 text-white flex-col'>
+      <div className='font-mono capitalize w-[60rem] projectList glassBox h-min mx-auto my-36 '>
+        <ul onMouseLeave={()=>(setIsMouseOver(null))} className='flex h-min gap-4 p-6 text-white flex-col'>
           <h1 className='mx-auto mt-0 font-mono'>P R O J E C T S</h1>
           {projects.map((project, id) => (
             <li
+
               key={id}
-              className={`glassBox ccc p-2 flex  transition-all  flex-col`}
+              className={`glassBox ccc p-2 flex  transition-all flex-col`}
               id={`${project}`}
-              onMouseOver={() => setIsMouseOver(project)}
-              onMouseOut={() => setIsMouseOver(null)}
+              onClick={()=>setIsMouseOver(project)}
             >
-              <h1>{project}</h1>
+              <h1 className='flex justify-between'>
+              {project}
+              <i
+               onClick={()=>setIsMouseOver(project)}
+               onDoubleClick={()=>(setIsMouseOver(null))}
+               className="ri-arrow-left-circle-line  focus-within:rotate-[270deg] text-xl">
+               </i>
+              </h1>
               {isMouseOver === project && (
-                <div id={`${project}subFolder`} className={`glassBox flex  flex-col justify-center `}>
-                  <h1>Welcome to {project} Project</h1>
-                  <h2>Coming Soon</h2>
+                <div id={`${project}subFolder`} className={`glassBox flex  flex-col justify-center  mt-1`}>
+
                 </div>
               )}
             </li>
