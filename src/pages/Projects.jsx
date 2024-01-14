c77f8736a6fd25dc1e410a5486510e501046c4b2
Harshit Prajapati <harshitprajapati5114@gmail.com>
2024-01-14 14:13:57 +0530

    minor changes

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,42 +1,28 @@
-import React, { useState } from 'react';
+import React from 'react'
 
-
-const Projects = ({ projects = ["redux", "react"] }) => {
-  const [isMouseOver, setIsMouseOver] = useState(null);
+const Projects = () => {
   
 
+
   return (
-    <>
-      <div className='font-mono capitalize w-[60rem] projectList glassBox h-min mx-auto my-36 '>
-        <ul onMouseLeave={()=>(setIsMouseOver(null))} className='flex h-min gap-4 p-6 text-white flex-col'>
-          <h1 className='mx-auto mt-0 font-mono'>P R O J E C T S</h1>
-          {projects.map((project, id) => (
-            <li
+    <div className='text-white flex w-max mx-auto my-24   '>
+      <ul  className=' flex flex-col w-[50rem] h-min  glassBox  text-center font-mono '>
+      <h1 className='text-center mt-2 font-mono'> P R O J E C T S  </h1> 
+      <li className=' glassBox ccc p-2 m-3 '>
+        <h1>React</h1>
+        {true && <div className='reactSF glassBox'></div>}
+      </li>
+      <li className='glassBox ccc  p-2 m-3'>
+        <h1>Python</h1>
+        {true && <div className='reactSF glassBox'></div>}
+      </li>
+      <li className='glassBox ccc  p-2 m-3'>
+        <h1>C++</h1>
+        {true && <div className='reactSF glassBox'></div>}
+      </li>
+      </ul>
+    </div>
+  )
+}
 
-              key={id}
-              className={`glassBox ccc p-2 flex  transition-all flex-col`}
-              id={`${project}`}
-              onClick={()=>setIsMouseOver(project)}
-            >
-              <h1 className='flex justify-between'>
-              {project}
-              <i
-               onClick={()=>setIsMouseOver(project)}
-               onDoubleClick={()=>(setIsMouseOver(null))}
-               className="ri-arrow-left-circle-line  focus-within:rotate-[270deg] text-xl">
-               </i>
-              </h1>
-              {isMouseOver === project && (
-                <div id={`${project}subFolder`} className={`glassBox flex  flex-col justify-center  mt-1`}>
-
-                </div>
-              )}
-            </li>
-          ))}
-        </ul>
-      </div>
-    </>
-  );
-};
-
-export default Projects;
+export default Projects
