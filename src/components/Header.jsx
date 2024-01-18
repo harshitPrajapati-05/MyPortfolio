67b7ebb910ccd8be52f344bd8b94e6c6f8b9d050
Harshit Prajapati <harshitprajapati5114@gmail.com>
2024-01-18 17:37:18 +0530

    changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link, NavLink } from 'react-router-dom';
 import ImgLoader from './ImgLoader'
 import { useSelector } from 'react-redux';
+import {gsap} from 'gsap';
 const Header = (
   {
     name=" Harshit A Prajapati",
@@ -11,18 +12,18 @@ const Header = (
   ) => {
     const toMaker = to => { return `/${to.toString().toLowerCase()}` } ;
     const userName = useSelector(state=>state.user.name)
-    
+
 
   return (   
     <>
-    <div className={`flex flex-wrap  justify-between h-min  w-full font-mono bg-transparent text-white`}>
+    <div id="header" className={`flex flex-wrap  justify-between h-min  w-full font-mono bg-transparent text-white`}>
     <div className='flex  font-mono text-[#F5AC0C]  m-2'>
     <Link to="https://www.instagram.com/harshit_prajapati04/">
     {userName!==""?<ImgLoader src={`../hars.jpg`} className={`h-20 rounded-full`} name={name} descp={descp}/>:null}
     </Link>
     </div>
-    <ul className={`flex flex-wrap gap-[1.75rem]`}>
-    {contexts.map((context ,index)=>(
+    <ul  className={`flex flex-wrap gap-[1.75rem]`}>
+    {userName!== "" && contexts.map((context ,index)=>(
      <li    key={index} className='mt-1 mx-[1.25rem] text-lg font-mono font-semibold '>
       <NavLink  to={toMaker(`${context}`)}
       className={({isActive})=>`${isActive? "text-grey glassBox bg-transparent shadow-[1px_1px_15px_1px_rgba(146,46,227,255)]   ":"  glow hover:transition-all hover:shadow-[5px_5px_1px_0px_rgba(146,46,227,255)]"}    hover:text-white hover:bg-transparent transition-shadow   glassBox flex rounded-lg p-[0.25rem] my-5`}
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,24 +1,68 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Projects = () => {
-  
+  const [isReact, setIsReact] = useState(false)
+  const [isPython, setIsPython] = useState(false)
+  const [isCPP, setIsCPP] = useState(false)
+  const megaFolder =()=>{
+    setTimeout(() => {
+      setIsReact(false);
+      setIsPython(false);
+      setIsCPP(false);
+    }, 500);
+  }
 
+  
+  useEffect(()=>{
+    if(isReact==true){
+      setIsPython(false)
+      setIsCPP(false)
+    }
+    if(isPython==true){
+      setIsReact(false)
+      setIsCPP(false)
+    }
+    if(isCPP==true){
+      setIsPython(false)
+      setIsReact(false)
+    }
+  },[isReact, isPython , isCPP])
 
   return (
     <div className='text-white flex w-max mx-auto my-24   '>
-      <ul  className=' flex flex-col w-[50rem] h-min  glassBox  text-center font-mono '>
-      <h1 className='text-center mt-2 font-mono'> P R O J E C T S  </h1> 
-      <li className=' glassBox ccc p-2 m-3 '>
-        <h1>React</h1>
-        {true && <div className='reactSF glassBox'></div>}
+      <ul  className=' flex flex-col w-[55rem] h-min  glassBox p-4  text-center font-mono '>
+      <h1 className='text-center mt-1 font-mono'> P R O J E C T S  </h1> 
+      <li
+      onClick={()=>setIsReact(true)}
+
+      className=' glassBox ccc p-2 m-3 '>
+        <h1 onDoubleClick={()=>setIsReact(false)}>React</h1>
+        {isReact && <div className='reactSF p-4 glassBox'>
+        <ul>
+        <li >
+          <div>
+            <h1 className=' capitalize'>this is based on ReactJS </h1>
+            <div className='flex justify-between'>
+              <p>lorem</p>
+              <img src='../pre2.PNG' className='h-28' alt='hler'/>
+            </div>
+          </div>
+        </li>
+        </ul>
+        </div>}
       </li>
-      <li className='glassBox ccc  p-2 m-3'>
-        <h1>Python</h1>
-        {true && <div className='reactSF glassBox'></div>}
+      <li 
+      onClick={()=>setIsPython(true)}
+      
+      className='glassBox ccc  p-2 m-3'>
+        <h1 onDoubleClick={()=>setIsPython(false)}>Python</h1>
+        {isPython && <div className='pythonSF glassBox'></div>}
       </li>
-      <li className='glassBox ccc  p-2 m-3'>
-        <h1>C++</h1>
-        {true && <div className='reactSF glassBox'></div>}
+      <li
+      onClick={()=>setIsCPP(true)}
+      className='glassBox ccc  p-2 m-3'>
+        <h1 onDoubleClick={()=>setIsCPP(false)}>C++</h1>
+        {isCPP && <div className='cppSF glassBox'></div>}
       </li>
       </ul>
     </div>
